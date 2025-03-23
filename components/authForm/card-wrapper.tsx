49e2bc0775f7f9f883ce2fa1473709360401940c
aledx18 <alediiaz18@gmail.com>
2025-03-23 14:21:12 -0300

    refactor(ui): update styles and layout for `navUser`, `card-wrapper`, and `social` components

--- components/authForm/card-wrapper.tsx
+++ components/authForm/card-wrapper.tsx
@@ -28,6 +28,11 @@ export default function CardWrapper({
         <Header label={headerLabel} />
       </CardHeader>
       <CardContent>{children}</CardContent>
+      <div className='relative text-center mx-6 text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
+        <span className='relative z-10 bg-card px-2 text-muted-foreground'>
+          Or
+        </span>
+      </div>
       {showSocial && (
         <CardFooter>
           <Social />
--- components/sidabar/nav-user.tsx
+++ components/sidabar/nav-user.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { ChevronsUpDown } from 'lucide-react'
-
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 import {
@@ -10,15 +9,11 @@ import {
   SidebarMenuItem
 } from '@/components/ui/sidebar'
 
-export function NavUser({
-  user
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
-  }
-}) {
+import { Database } from '@/db_types'
+
+type ProfileData = Database['public']['Tables']['profiles']['Row'] | null
+
+export function NavUser({ profileData }: { profileData: ProfileData }) {
   return (
     <SidebarMenu>
       <SidebarMenuItem>
@@ -26,12 +21,19 @@ export function NavUser({
           size='lg'
           className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'>
           <Avatar className='h-8 w-8 rounded-lg'>
-            <AvatarImage src={user.avatar} alt={user.name} />
-            <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
+            <AvatarImage
+              src={profileData?.avatar || ''}
+              alt={profileData?.first_name || ''}
+            />
+            <AvatarFallback className='rounded-lg'>
+              {profileData?.first_name?.slice(0, 2)}
+            </AvatarFallback>
           </Avatar>
           <div className='grid flex-1 text-left text-sm leading-tight'>
-            <span className='truncate font-semibold'>{user.name}</span>
-            <span className='truncate text-xs'>{user.email}</span>
+            <span className='truncate font-semibold'>
+              {profileData?.first_name}
+            </span>
+            <span className='truncate text-xs'>{profileData?.email}</span>
           </div>
           <ChevronsUpDown className='ml-auto size-4' />
         </SidebarMenuButton>
