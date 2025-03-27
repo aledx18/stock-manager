016de6a6716034cf59e2fa71276859529f2da317
aledx18 <alediiaz18@gmail.com>
2025-03-27 15:16:37 -0300

    Add: const pageLinks

---
+++ components/sidebar/pageLinks.ts
@@ -0,0 +1,19 @@
+import { Search, Settings, Users } from 'lucide-react'
+
+export const pageLinks = [
+  {
+    name: 'Users',
+    url: '#',
+    icon: Users
+  },
+  {
+    name: 'Search',
+    url: '#',
+    icon: Search
+  },
+  {
+    name: 'Settings',
+    url: '#',
+    icon: Settings
+  }
+]
