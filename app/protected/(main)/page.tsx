48ab212f058f8bd631eb87d08892bb93d6e93637
aledx18 <alediiaz18@gmail.com>
2025-04-04 15:30:49 -0300

    Refactor(protected): add ProtectedPage component for company management dashboard

--- app/protected/(main)/page.tsx
+++ app/protected/(main)/page.tsx
@@ -9,8 +9,6 @@ import {
 import { Badge } from '@/components/ui/badge'
 import { TrendingDown, TrendingUp } from 'lucide-react'
 import Company from '@/components/protectedMain/company'
-import { Suspense } from 'react'
-import { Skeleton } from '@/components/ui/skeleton'
 
 export default function ProtectedPage() {
   return (
@@ -18,9 +16,7 @@ export default function ProtectedPage() {
       <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
         {/* <SectionCards /> */}
         <div className='px-4 lg:px-6'>
-          <Suspense fallback={<Skeleton className='h-8 w-32' />}>
-            <Company />
-          </Suspense>
+          <Company />
           <p className='text-muted-foreground text-sm'>Manage your Company</p>
         </div>
         <div className='*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4'>
