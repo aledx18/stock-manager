587b340bc8039a676558649be7eedc3505e81a52
aledx18 <alediiaz18@gmail.com>
2025-04-04 15:37:21 -0300

    Refactor(users): add CellAction component for user actions in the user management interface

---
+++ app/protected/users/components/cell-action.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { Button } from '@/components/ui/button'
+import { Edit, MoreHorizontal, Trash } from 'lucide-react'
+
+import { useState } from 'react'
+import { AlertModal } from '@/components/modals/alert-modal'
+
+// interface CellActionProps {
+//   data: ProductColumn
+// }
+
+export default function CellAction() {
+  const [open, setOpen] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const onDelete = async () => {
+    setLoading(true)
+
+    setLoading(false)
+    setOpen(false)
+  }
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        loading={loading}
+        onConfirm={onDelete}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='h-8 w-8 p-0'>
+            <span className='sr-only'>Open menu</span>
+            <MoreHorizontal className='h-4 w-4' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end'>
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Edit className='mr-2 h-4 w-4' />
+            Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className='mr-2 h-4 w-4' />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
